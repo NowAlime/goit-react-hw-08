1ed869c831ef1028996ddd9093eb2098650caba8
NowAlime <147753857+NowAlime@users.noreply.github.com>
2024-06-03 14:10:19 +0300

    add

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from '../src/components/App/App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App/App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store, persist } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persist}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
