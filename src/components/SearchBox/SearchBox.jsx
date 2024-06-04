48f761b258e2177e69b36b36c790c2f575a08d52
NowAlime <147753857+NowAlime@users.noreply.github.com>
2024-06-04 17:18:25 +0300

    add

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,21 +1,19 @@
 import { useId } from "react";
+import style from "../SearchBox/SearchBox.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { selectNameFilter } from "../../redux/selectors";
-import { setStatusFilter } from "../../redux/filtersSlice";
-import style from "./SearchBox.module.css";
+import { changeFilter } from "../../redux/filtersSlice";
 
-
- function SearchBox() {
+function SearchBox() {
   const finedId = useId();
-
   const dispatch = useDispatch();
-
   const filter = useSelector(selectNameFilter);
 
   const handleFilter = (e) => {
     const name = e.target.value.trim();
-    dispatch(setStatusFilter(name));
+    dispatch(changeFilter(name));
   };
+
   return (
     <div className={style.filter}>
       <label htmlFor={finedId}>Find contacts by name</label>
@@ -26,7 +24,6 @@ import style from "./SearchBox.module.css";
         value={filter}
         onChange={handleFilter}
         name="name"
-        placeholder="Search..."
       />
     </div>
   );
--- src/redux/filtersSlice.js
+++ src/redux/filtersSlice.js
@@ -3,13 +3,14 @@ import { createSlice } from "@reduxjs/toolkit";
 const filterSlice = createSlice({
   name: "filters",
   initialState: {
-    status: "",
+    name: "",
   },
   reducers: {
-    setStatusFilter: (state, action) => {
-      state.status = action.payload;
+    changeFilter: (state, action) => {
+      state.name = action.payload;
     },
   },
 });
-export const { setStatusFilter } = filterSlice.actions;
+
+export const { changeFilter } = filterSlice.actions;
 export const filterReducer = filterSlice.reducer;
